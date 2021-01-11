4703553a037c29a436688fb5243b0f6004fb8e5b
Cassandra <cparisi1290@gmail.com>
2021-01-11 17:24:21 -0500

    made continuious

--- lib/perma_plan_cli/cli.rb
+++ lib/perma_plan_cli/cli.rb
@@ -2,23 +2,22 @@ class PermaPlanCli::CLI
     attr_accessor :sorted_books
 
     def call
-        tractor = "\u{1F69C}"
-        
-        puts Rainbow("\nWelcome to the Perma-Planning Resource App!\n").deepskyblue
-        sleep(1)
-        puts Rainbow("\nHere are your choice of permaculture books to start on your homestead!").deepskyblue
-        sleep(1)
-        puts "                                   " + tractor
-        sleep(1)
-        puts "                               " + tractor
-        sleep(1)
-        puts Rainbow("                           " + tractor + "     "+"Stuck behind a tractor...").palevioletred
-        sleep(1)
-        puts "                       " + tractor
-        sleep(1)
-        puts "                   " + tractor
-        sleep(1)
-        puts Rainbow("              " + tractor + "       "    "Your list is coming up...").palevioletred
+        # tractor = "\u{1F69C}"
+        # puts Rainbow("\nWelcome to the Perma-Planning Resource App!\n").deepskyblue
+        # sleep(1)
+        # puts Rainbow("\nHere are your choice of permaculture books to start on your homestead!").deepskyblue
+        # sleep(1)
+        # puts "                                   " + tractor
+        # sleep(1)
+        # puts "                               " + tractor
+        # sleep(1)
+        # puts Rainbow("                           " + tractor + "     "+"Stuck behind a tractor...").palevioletred
+        # sleep(1)
+        # puts "                       " + tractor
+        # sleep(1)
+        # puts "                   " + tractor
+        # sleep(1)
+        # puts Rainbow("              " + tractor + "       "    "Your list is coming up...").palevioletred
         
         PermaPlanCli::Scraper.scrape_books
         sort_books
@@ -27,15 +26,14 @@ class PermaPlanCli::CLI
     end
 
     def sort_books        
-        @sorted_books = PermaPlanCli::Book.all.sort_by {|book| book.title}
-        
+        @sorted_books = PermaPlanCli::Book.all.sort_by {|book| book.title}  
     end
 
     def list_books
         @sorted_books.each.with_index(1) do |book, index|
-            puts Rainbow("\n#{index}. #{book.title}\n").mediumpurple
-            puts Rainbow("By: #{book.author}").mediumpurple
-            puts Rainbow("Price: #{book.price}").mediumpurple
+            puts Rainbow("\n#{index}. #{book.title}").mediumpurple
+            puts Rainbow("    by: #{book.author}").mediumpurple
+            puts Rainbow("    price: #{book.price}").mediumpurple
         end
     end
 
@@ -43,8 +41,9 @@ class PermaPlanCli::CLI
         puts Rainbow("\nPlease select a number to find out more information about the book or type 'exit' to quit.\n").deepskyblue
         input = gets.strip
         until input.to_i.between?(0, list_books.length) || input == "exit"
-            puts Rainbow("\nSorry! I don't understand that command.\n").deepskyblue
-            input gets.strip
+            puts Rainbow("\nSorry! I don't understand that command.").deepskyblue 
+            puts Rainbow("Please select a valid number or type exit to quit.\n").deepskyblue
+            input = gets.strip
         end
                 
         if input != "exit" 
@@ -80,17 +79,16 @@ class PermaPlanCli::CLI
       ... `'-._|_|;:;_.-'` '::.  `"-
        .:;.      .:.   ::.     '::."
        EOF
-
+  
         end
     end
 
     def want_more_info(book_object)
-        puts Rainbow("\nRead more? (Y/N)\n").deepskyblue
+        puts Rainbow("\n Type YES for more information - type 'list' to see the selection of books again.\n").deepskyblue
         input = gets.strip.upcase
-        until ["Y", "N", "YES", "NO"].include?(input)
-            puts Rainbow("Please type Y or N to continue.").deepskyblue
+        until ["Y", "YES", "list", "LIST", "EXIT", "exit"].include?(input)
+            puts Rainbow("Please type YES or LIST to continue or exit to quit.").deepskyblue
             input = gets.strip.upcase
-            puts Rainbow("Type Y/N to continue.").deepskyblue
         end
         if input == "Y" || input == "YES"
             puts Rainbow("\n...Fetching more info...\n").deepskyblue
@@ -98,7 +96,51 @@ class PermaPlanCli::CLI
             puts Rainbow("#{book_object.title}").mediumpurple
             puts Rainbow("by:#{book_object.author}\n").mediumpurple
             puts Rainbow("#{book_object.description}\n").mediumpurple
-            get_book_method  
+            show_book_list_again    
+            # get_book_method  
+        elsif input == "list" || "LIST"
+            list_books
+            get_book_method
+        elsif input == "EXIT" || "exit"
+            puts ""
+            puts Rainbow("\nSee you on the homestead!\n").deepskyblue
+            puts ""
+            puts <<-'EOF'
+                       '
+            .      '      .
+      .      .     :     .      .
+       '.        ______       .'
+         '  _.-"`      `"-._ '
+          .'                '.
+   `'--. /                    \ .--'`
+        /                      \
+       ;                        ;
+  - -- |                        | -- -
+       |     _.                 |
+       ;    /__`A   ,_          ;
+   .-'  \   |= |;._.}{__       /  '-.
+      _.-""-|.' # '. `  `.-"{}<._
+            / 1990  \     \  x   `"
+       ----/         \_.-'|--X----
+       -=_ |         |    |- X.  =_
+      - __ |_________|_.-'|_X-X##
+      ... `'-._|_|;:;_.-'` '::.  `"-
+       .:;.      .:.   ::.     '::."
+       EOF
+        end
+    end
+
+    def show_book_list_again
+        puts Rainbow("Type 'list' to see the selection of books again - type 'exit' to quit.\n").deepskyblue
+        show_book_list_again_input = gets.strip.upcase
+        until ["list", "LIST"].include?(show_book_list_again_input)
+            puts Rainbow("Please type LIST to continue or exit to quit.").deepskyblue
+            show_book_list_again_input = gets.strip.upcase
+        end
+        if show_book_list_again_input == "LIST" || show_book_list_again_input == "list"
+            list_books
+            get_book_method
+        # elsif show_book_list_again_input == "EXIT" || "exit"
         else
             puts ""
             puts Rainbow("\nSee you on the homestead!\n").deepskyblue
@@ -125,7 +167,6 @@ class PermaPlanCli::CLI
       ... `'-._|_|;:;_.-'` '::.  `"-
        .:;.      .:.   ::.     '::."
        EOF
-
         end
     end
 end
