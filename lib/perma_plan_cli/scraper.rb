f38d464c8c8bed7a76ca46356746b49b325bc5cc
Cassandra <cparisi1290@gmail.com>
2021-01-17 09:47:14 -0500

    refractored

--- lib/perma_plan_cli/scraper.rb
+++ lib/perma_plan_cli/scraper.rb
@@ -7,22 +7,6 @@ class PermaPlanCli::Scraper
         page = Nokogiri::HTML(open("#{BASE_URL}/books?keywords=permaculture"))
 
         array_of_books = page.css("div.booklist-book")
-        
-        # array_of_books.each do |book_square|
-
-        #     if book_square.css("h2.font-serif-bold a")[0].attributes['href'].value != nil
-        #         url = BASE_URL + book_square.css("h2.font-serif-bold a")[0].attributes['href'].value 
-        #     else
-        #         url = nil
-        #     end
-
-        #     title = book_square.css("h2.font-serif-bold").text.strip
-        #     author = book_square.css("h3.text-s").text.strip
-        #     price = book_square.css("div.pb-4 div.font-sans-bold").text.strip
-        
-        # book = PermaPlanCli::Book.new(title, author, price, url)
-
-        # end
 
         array_of_books.map do |book_square|
             {
@@ -45,3 +29,19 @@ class PermaPlanCli::Scraper
 
 end
 
+
+        # array_of_books.each do |book_square|
+
+        #     if book_square.css("h2.font-serif-bold a")[0].attributes['href'].value != nil
+        #         url = BASE_URL + book_square.css("h2.font-serif-bold a")[0].attributes['href'].value 
+        #     else
+        #         url = nil
+        #     end
+
+        #     title = book_square.css("h2.font-serif-bold").text.strip
+        #     author = book_square.css("h3.text-s").text.strip
+        #     price = book_square.css("div.pb-4 div.font-sans-bold").text.strip
+        
+        # book = PermaPlanCli::Book.new(title, author, price, url)
+
+        # end
